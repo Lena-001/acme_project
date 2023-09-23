202cf9d6b08a17d7a14301ddd03dbe92b8298a6f
Елена Губаева <el.gubaeva1909@gmail.com>
2023-09-23 22:25:10 +0300

    Спринт 7

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -2,14 +2,17 @@
 from django.db import models
 # Импортируется функция-валидатор.
 from .validators import real_age
+# Импортируем функцию reverse() для получения ссылки на объект.
+from django.urls import reverse
 
 
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
     last_name = models.CharField(
-        'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
+        'Фамилия', max_length=20, help_text='Опциональное поле', blank=True
     )
     birthday = models.DateField('Дата рождения', validators=(real_age,))
+    image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
 
     class Meta:
         constraints = (
@@ -18,4 +21,8 @@ class Birthday(models.Model):
                 name='Unique person constraint',
             ),
         )
+
+    def get_absolute_url(self):
+        # С помощью функции reverse() возвращаем URL объекта.
+        return reverse('birthday:detail', kwargs={'pk': self.pk})
  
